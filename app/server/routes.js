d7c772cd560d345f488232f99de1a4e40df537ef
Varun Arora <varun.arora@vanderbilt.edu>
2017-07-21 10:56:54 -0700

    more ES6-ifying

--- app/server/routes.js
+++ app/server/routes.js
@@ -2,18 +2,18 @@ const User = require('./models/User');
 
 module.exports = function(app) {
   // Application ------------------------------------------
-  app.get('/', function(req, res) {
+  app.get('/', (req, res) => {
     res.sendfile('./app/client/index.html');
   });
 
-  app.get('/mlh', function(req, res) {
+  app.get('/mlh', (req, res) => {
     res.redirect('https://my.mlh.io/oauth/authorize?client_id=' +
     process.env.MLH_APP_ID +
     '&redirect_uri=http%3A%2F%2Flocalhost%3A3000%2Fapplication&response_type=token');
   });
 
   // Wildcard all other GET requests to the angular app
-  app.get('*', function(req, res) {
+  app.get('*', (req, res) => {
     res.sendfile('./app/client/index.html');
   });
 };
